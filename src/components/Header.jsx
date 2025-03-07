31a7508d878f4bcff7607189ca4d488a9a0650d3
AzizAVERIBOU <azizaveribou6123@gmail.com>
2025-03-07 18:19:18 -0500

    Modifications du Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,6 +28,7 @@ const Header = () => {
             <li><NavLink className="text-dark" to="/formation" onClick={toggleMenu}>Formation</NavLink></li>
             <li><NavLink className="text-dark" to="/admission" onClick={toggleMenu}>Admission</NavLink></li>
             <li><NavLink className="text-dark" to="/actualite" onClick={toggleMenu}>Actualité</NavLink></li>
+            <li><NavLink className="text-dark" to="/mediatheque" onClick={toggleMenu}>Médiathèque</NavLink></li>
             <li><NavLink className="text-dark" to="/alumni" onClick={toggleMenu}>Alumni</NavLink></li>
             <li><NavLink className="text-dark" to="/contact" onClick={toggleMenu}>Contact</NavLink></li>
           </ul>
