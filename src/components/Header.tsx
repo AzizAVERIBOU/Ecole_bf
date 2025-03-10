bf8b2d3e30de59e49eae87924018d59007656a8a
Zeh-Eox <arnolddevmail@gmail.com>
2025-03-10 14:19:48 +0000

    minor changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import '../styles/Header.css';
 import { FiMenu, FiX } from 'react-icons/fi';
+import ROUTES from '../routes/path';
 
 const Header: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
@@ -23,14 +24,14 @@ const Header: React.FC = () => {
         </button>
         <nav className={`navigation bg-light ${menuOpen ? 'open' : ''} d-flex justify-content-center align-items-center gap-5`}>
           <ul>
-            <li><NavLink className="text-dark" to="/" end onClick={toggleMenu}>Accueil</NavLink></li>
-            <li><NavLink className="text-dark" to="/about" onClick={toggleMenu}>À propos</NavLink></li>
-            <li><NavLink className="text-dark" to="/formation" onClick={toggleMenu}>Formation</NavLink></li>
-            <li><NavLink className="text-dark" to="/admission" onClick={toggleMenu}>Admission</NavLink></li>
-            <li><NavLink className="text-dark" to="/actualite" onClick={toggleMenu}>Actualité</NavLink></li>
-            <li><NavLink className="text-dark" to="/mediatheque" onClick={toggleMenu}>Médiathèque</NavLink></li>
-            <li><NavLink className="text-dark" to="/alumni" onClick={toggleMenu}>Alumni</NavLink></li>
-            <li><NavLink className="text-dark" to="/contact" onClick={toggleMenu}>Contact</NavLink></li>
+            <li><NavLink className="text-dark" to={ROUTES.HOME} end onClick={toggleMenu}>Accueil</NavLink></li>
+            <li><NavLink className="text-dark" to={ROUTES.ABOUT} onClick={toggleMenu}>À propos</NavLink></li>
+            <li><NavLink className="text-dark" to={ROUTES.FORMATION} onClick={toggleMenu}>Formation</NavLink></li>
+            <li><NavLink className="text-dark" to={ROUTES.ADMISSION} onClick={toggleMenu}>Admission</NavLink></li>
+            <li><NavLink className="text-dark" to={ROUTES.ACTUALITE} onClick={toggleMenu}>Actualité</NavLink></li>
+            <li><NavLink className="text-dark" to={ROUTES.MEDIATHEQUE} onClick={toggleMenu}>Médiathèque</NavLink></li>
+            <li><NavLink className="text-dark" to={ROUTES.ALUMNI} onClick={toggleMenu}>Alumni</NavLink></li>
+            <li><NavLink className="text-dark" to={ROUTES.CONTACT} onClick={toggleMenu}>Contact</NavLink></li>
           </ul>
         </nav>
       </div>
