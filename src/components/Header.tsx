fb7f80c41f288c2c12a9f7c1bc3abb4958bb6278
AmadouGeek <amhedbaron@gmail.com>
2025-05-20 15:02:58 +0100

    ajout amadou

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import '../styles/Header.css';
 import { FiMenu, FiX } from 'react-icons/fi';
+import { FaCheckCircle } from 'react-icons/fa';
+import '../styles/Header.css';
 import ROUTES from '../routes/path';
 
 const Header: React.FC = () => {
@@ -14,24 +15,38 @@ const Header: React.FC = () => {
   return (
     <header className="header bg-light">
       <div className="header-content">
-        <div className="logo">
+        <div className="logo-container">
           <Link to="/">
             <img src="/images/logo-cpge.jpg" alt="CPGE MENAPLN" className="header-logo" />
           </Link>
         </div>
-        <button className="menu-toggle text-dark" onClick={toggleMenu}>
+        <button className="menu-toggle" onClick={toggleMenu}>
           {menuOpen ? <FiX /> : <FiMenu />}
         </button>
-        <nav className={`navigation bg-light ${menuOpen ? 'open' : ''} d-flex justify-content-center align-items-center gap-5`}>
+        <nav className={`navigation ${menuOpen ? 'open' : ''}`}>
           <ul>
-            <li><NavLink className="text-dark" to={ROUTES.HOME} end onClick={toggleMenu}>Accueil</NavLink></li>
-            <li><NavLink className="text-dark" to={ROUTES.ABOUT} onClick={toggleMenu}>À propos</NavLink></li>
-            <li><NavLink className="text-dark" to={ROUTES.FORMATION} onClick={toggleMenu}>Formation</NavLink></li>
-            <li><NavLink className="text-dark" to={ROUTES.ADMISSION} onClick={toggleMenu}>Admission</NavLink></li>
-            <li><NavLink className="text-dark" to={ROUTES.ACTUALITE} onClick={toggleMenu}>Actualité</NavLink></li>
-            <li><NavLink className="text-dark" to={ROUTES.MEDIATHEQUE} onClick={toggleMenu}>Médiathèque</NavLink></li>
-            <li><NavLink className="text-dark" to={ROUTES.ALUMNI} onClick={toggleMenu}>Alumni</NavLink></li>
-            <li><NavLink className="text-dark" to={ROUTES.CONTACT} onClick={toggleMenu}>Contact</NavLink></li>
+            {[
+              { name: 'Accueil', path: ROUTES.HOME },
+              { name: 'À propos', path: ROUTES.ABOUT },
+              { name: 'Formation', path: ROUTES.FORMATION },
+              { name: 'Admission', path: ROUTES.ADMISSION },
+              { name: 'Actualité', path: ROUTES.ACTUALITE },
+              { name: 'Médiathèque', path: ROUTES.MEDIATHEQUE },
+              { name: 'Alumni', path: ROUTES.ALUMNI },
+              { name: 'Contact', path: ROUTES.CONTACT },
+            ].map((item, index) => (
+              <li key={index}>
+                <NavLink
+                  to={item.path}
+                  end={item.path === ROUTES.HOME}
+                  onClick={toggleMenu}
+                  className="nav-link"
+                >
+                  
+                  {item.name}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
